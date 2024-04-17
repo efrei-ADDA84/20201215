9c700e5f81a7fab3e3326e32f906cf035dee7ced
Profil ALEXIS <alexis.ung@efrei.net>
2024-04-17 19:47:09 +0200

    df

--- weather_API.py
+++ weather_API.py
@@ -60,4 +60,4 @@ def get_weather():
         return jsonify({"error": "Weather data not found."}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
